b7e2920105c8491392fa5f3898d5c6b7265137c9
ka-tan <turns.forms.0g@icloud.com>
2025-06-07 16:31:52 +0800

    fixed strtrim and calloc

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ka-tan <ka-tan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 17:42:37 by ka-tan            #+#    #+#             */
-/*   Updated: 2025/05/16 20:31:59 by ka-tan           ###   ########.fr       */
+/*   Updated: 2025/06/07 16:06:42 by ka-tan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,35 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
+	size_t	total;
 
+	if (nmemb == 0 || size == 0)
+		return (malloc(1));
+	total = nmemb * size;
+	if (nmemb != 0 && total / nmemb != size)
+		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
-	else
-	{
-		ft_memset(ptr, 0, nmemb * size);
-	}
+	ft_memset(ptr, 0, nmemb * size);
 	return (ptr);
 }
-/*
-int main(void)
-{
-	size_t num_elements = 5;
-	 // Allocate an array of 5 integers using ft_calloc
-	int *array = (int *)ft_calloc(num_elements, sizeof(int));
-   // Check if the allocation was successful
-	if (array == NULL)
-	{
-		printf("Memory allocation failed.\n");
-		return 1;
-	}
-    // Print the allocated and zero-initialized array elements
-    for (size_t i = 0; i < num_elements; i++) 
-	{
-	        printf("array[%zu] = %d\n", i, array[i]);
-    }
-    // Free the allocated memory
-    free(array);
-    return 0;
-}
-*/
+
+// int main(void)
+// {
+// 	size_t num_elements = 5;
+// 	int *array = (int *)ft_calloc(num_elements, sizeof(int));
+// 	size_t i = 0; 
+// 	if (array == NULL)
+// 	{
+// 		printf("Memory allocation failed.\n");
+// 		return 1;
+// 	}
+//     while (i < num_elements) 
+// 	{
+// 		i++;
+// 		printf("array[%zu] = %d\n", i, array[i]);
+//     }
+//     free(array);
+//     return 0;
+// }
--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ka-tan <ka-tan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 13:42:06 by ka-tan            #+#    #+#             */
-/*   Updated: 2025/05/16 20:34:55 by ka-tan           ###   ########.fr       */
+/*   Updated: 2025/06/07 16:15:57 by ka-tan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t n)
 {
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	dlen;
-	unsigned int	slen;
+	size_t	i;
+	size_t	j;
+	size_t	dlen;
+	size_t	slen;
 
+	if (!dest || !src)
+		return (0);
 	i = 0;
 	j = 0;
 	while (dest[j] != '\0')
@@ -36,13 +38,13 @@ size_t	ft_strlcat(char *dest, const char *src, size_t n)
 	dest[j] = '\0';
 	return (dlen + slen);
 }
-/*
-int	main(void)
-{
-	char	src[] = "#Break The Code";
-	char	dest[] = "Champions are those who never quit.";
 
-	printf("src: %s\n dest: %s\n", src, dest);
-	printf("size of: %zu\n", ft_strlcat(dest, src, 55));
-	printf("src: %s\n dest: %s\n", src, dest);
-}*/
+// int	main(void)
+// {
+// 	char	src[] = "#Break The Code";
+// 	char	dest[] = "Champions are those who never quit.";
+
+// 	printf("src: %s\n dest: %s\n", src, dest);
+// 	printf("size of: %zu\n", ft_strlcat(dest, src, 55));
+// 	printf("src: %s\n dest: %s\n", src, dest);
+// }
